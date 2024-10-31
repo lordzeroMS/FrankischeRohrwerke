f217f76d07e0740dd2252debce423550476e8d15
LordZeroMS <github_code@lordzero.de>
2024-10-31 10:17:56 +0100

    fix error from adding sensors #4

--- custom_components/lueftungsanlage/number.py
+++ custom_components/lueftungsanlage/number.py
@@ -1,40 +1,34 @@
 import requests
 from homeassistant.components.number import NumberEntity
+from homeassistant.const import DEVICE_CLASS_POWER
 from .const import DOMAIN, CONF_IP_ADDRESS
 
 async def async_setup_entry(hass, entry, async_add_entities):
     ip_address = entry.data[CONF_IP_ADDRESS]
-    async_add_entities([LueftungsanlageRegler(hass, ip_address)])
+    async_add_entities([LueftungsanlageRegler(hass, ip_address, entry.entry_id)])
 
 class LueftungsanlageRegler(NumberEntity):
-    def __init__(self, hass, ip_address):
+    def __init__(self, hass, ip_address, entry_id):
         self._hass = hass
         self._ip_address = ip_address
-        self._value = 1  # Default stage
+        self._entry_id = entry_id
+        self._attr_native_value = 1  # Default stage
+        self._attr_native_min_value = 1
+        self._attr_native_max_value = 4
+        self._attr_native_step = 1
+        self._attr_device_class = DEVICE_CLASS_POWER
 
     @property
     def name(self):
         return "Lüftungsanlage Regler"
 
     @property
-    def min_value(self):
-        return 1
-
-    @property
-    def max_value(self):
-        return 4
-
-    @property
-    def step(self):
-        return 1
-
-    @property
-    def value(self):
-        return self._value
+    def unique_id(self):
+        return f"{self._entry_id}_regler"
 
     async def async_set_value(self, value):
         if 1 <= value <= 4:
             await self._hass.async_add_executor_job(
                 requests.get, f"http://{self._ip_address}/stufe.cgi?stufe={value}"
             )
-            self._value = value
+            self._attr_native_value = value
