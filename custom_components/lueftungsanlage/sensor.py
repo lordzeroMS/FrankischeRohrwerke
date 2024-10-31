dd3300a4b3f827b21c421350fa6449eb65cef1a7
LordZeroMS <github_code@lordzero.de>
2024-10-31 13:10:55 +0100

    fix blocking call in loop

--- custom_components/lueftungsanlage/sensor.py
+++ custom_components/lueftungsanlage/sensor.py
@@ -1,4 +1,5 @@
-import requests
+import aiohttp
+import async_timeout
 import xmltodict
 import time
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
@@ -7,22 +8,23 @@ from .const import DOMAIN, CONF_IP_ADDRESS
 async def async_setup_entry(hass, entry, async_add_entities):
     ip_address = entry.data[CONF_IP_ADDRESS]
     sensors = [
-        LueftungsanlageSensor(ip_address, "aktuell0", "Lüftungsanlage Stufe", entry.entry_id),
-        LueftungsanlageSensor(ip_address, "abl0", "Lüftungsanlage Abluft Temperatur", entry.entry_id),
-        LueftungsanlageSensor(ip_address, "zul0", "Lüftungsanlage Zuluft Temperatur", entry.entry_id),
-        LueftungsanlageSensor(ip_address, "aul0", "Lüftungsanlage Außenluft Temperatur", entry.entry_id),
-        LueftungsanlageSensor(ip_address, "fol0", "Lüftungsanlage Fortluft Temperatur", entry.entry_id),
-        LueftungsanlageSensor(ip_address, "rest_time", "Lüftungsanlage Filter Restzeit", entry.entry_id),
-        LueftungsanlageSensor(ip_address, "bypass", "Lüftungsanlage Bypass Status", entry.entry_id)
+        LueftungsanlageSensor(hass, ip_address, "aktuell0", "Lüftungsanlage Stufe", entry.entry_id),
+        LueftungsanlageSensor(hass, ip_address, "abl0", "Lüftungsanlage Abluft Temperatur", entry.entry_id),
+        LueftungsanlageSensor(hass, ip_address, "zul0", "Lüftungsanlage Zuluft Temperatur", entry.entry_id),
+        LueftungsanlageSensor(hass, ip_address, "aul0", "Lüftungsanlage Außenluft Temperatur", entry.entry_id),
+        LueftungsanlageSensor(hass, ip_address, "fol0", "Lüftungsanlage Fortluft Temperatur", entry.entry_id),
+        LueftungsanlageSensor(hass, ip_address, "rest_time", "Lüftungsanlage Filter Restzeit", entry.entry_id),
+        LueftungsanlageSensor(hass, ip_address, "bypass", "Lüftungsanlage Bypass Status", entry.entry_id)
     ]
     async_add_entities(sensors)
-    LueftungsanlageSensor.update_all_sensors(sensors)
+    await LueftungsanlageSensor.update_all_sensors(sensors)
 
 class LueftungsanlageSensor(SensorEntity):
     _shared_data = None
     _last_update = 0
 
-    def __init__(self, ip_address, sensor_type, name, entry_id):
+    def __init__(self, hass, ip_address, sensor_type, name, entry_id):
+        self._hass = hass
         self._ip_address = ip_address
         self._sensor_type = sensor_type
         self._name = name
@@ -43,13 +45,16 @@ class LueftungsanlageSensor(SensorEntity):
         return f"{self._entry_id}_{self._sensor_type}"
 
     @classmethod
-    def update_all_sensors(cls, sensors):
+    async def update_all_sensors(cls, sensors):
         current_time = time.time()
         if cls._shared_data is None or (current_time - cls._last_update) > 5:
-            res_xml = requests.get(f"http://{sensors[0]._ip_address}/status.xml")
-            if res_xml.status_code == 200:
-                cls._shared_data = xmltodict.parse(res_xml.text)['response']
-                cls._last_update = current_time
+            async with async_timeout.timeout(10):
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(f"http://{sensors[0]._ip_address}/status.xml") as response:
+                        if response.status == 200:
+                            text = await response.text()
+                            cls._shared_data = xmltodict.parse(text)['response']
+                            cls._last_update = current_time
         for sensor in sensors:
             sensor.update_from_shared_data()
 
@@ -60,5 +65,5 @@ class LueftungsanlageSensor(SensorEntity):
         else:
             self._state = raw_value
 
-    def update(self):
-        self.update_all_sensors([self])
+    async def async_update(self):
+        await self.update_all_sensors([self])
