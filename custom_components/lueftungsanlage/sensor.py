75199884594800aee06d70f013d1420666bee5fa
LordZeroMS <github_code@lordzero.de>
2024-10-31 10:28:42 +0100

    fix error from adding sensors #6

--- custom_components/lueftungsanlage/sensor.py
+++ custom_components/lueftungsanlage/sensor.py
@@ -1,26 +1,28 @@
 import requests
 import xmltodict
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
 from .const import DOMAIN, CONF_IP_ADDRESS
 
 async def async_setup_entry(hass, entry, async_add_entities):
     ip_address = entry.data[CONF_IP_ADDRESS]
     async_add_entities([
-        LueftungsanlageSensor(ip_address, "aktuell0", "Lüftungsanlage Stufe"),
-        LueftungsanlageSensor(ip_address, "abl0", "Lüftungsanlage Abluft Temperatur"),
-        LueftungsanlageSensor(ip_address, "zul0", "Lüftungsanlage Zuluft Temperatur"),
-        LueftungsanlageSensor(ip_address, "aul0", "Lüftungsanlage Außenluft Temperatur"),
-        LueftungsanlageSensor(ip_address, "fol0", "Lüftungsanlage Fortluft Temperatur"),
-        LueftungsanlageSensor(ip_address, "rest_time", "Lüftungsanlage Filter Restzeit"),
-        LueftungsanlageSensor(ip_address, "bypass", "Lüftungsanlage Bypass Status")
+        LueftungsanlageSensor(ip_address, "aktuell0", "Lüftungsanlage Stufe", entry.entry_id),
+        LueftungsanlageSensor(ip_address, "abl0", "Lüftungsanlage Abluft Temperatur", entry.entry_id),
+        LueftungsanlageSensor(ip_address, "zul0", "Lüftungsanlage Zuluft Temperatur", entry.entry_id),
+        LueftungsanlageSensor(ip_address, "aul0", "Lüftungsanlage Außenluft Temperatur", entry.entry_id),
+        LueftungsanlageSensor(ip_address, "fol0", "Lüftungsanlage Fortluft Temperatur", entry.entry_id),
+        LueftungsanlageSensor(ip_address, "rest_time", "Lüftungsanlage Filter Restzeit", entry.entry_id),
+        LueftungsanlageSensor(ip_address, "bypass", "Lüftungsanlage Bypass Status", entry.entry_id)
     ])
 
 class LueftungsanlageSensor(SensorEntity):
-    def __init__(self, ip_address, sensor_type, name):
+    def __init__(self, ip_address, sensor_type, name, entry_id):
         self._ip_address = ip_address
         self._sensor_type = sensor_type
         self._name = name
+        self._entry_id = entry_id
         self._state = None
+        self._attr_device_class = SensorDeviceClass.TEMPERATURE if "Temperatur" in name else None
 
     @property
     def name(self):
@@ -29,6 +31,10 @@ class LueftungsanlageSensor(SensorEntity):
     @property
     def state(self):
         return self._state
+
+    @property
+    def unique_id(self):
+        return f"{self._entry_id}_{self._sensor_type}"
 
     def update(self):
         res_xml = requests.get(f"http://{self._ip_address}/status.xml")
