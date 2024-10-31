bcda90849e038be85b9a5b17d85bef294fbb4e40
LordZeroMS <github_code@lordzero.de>
2024-10-31 12:56:10 +0100

    update sensors only once in 5 sec

--- custom_components/lueftungsanlage/sensor.py
+++ custom_components/lueftungsanlage/sensor.py
@@ -1,11 +1,12 @@
 import requests
 import xmltodict
+import time
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
 from .const import DOMAIN, CONF_IP_ADDRESS
 
 async def async_setup_entry(hass, entry, async_add_entities):
     ip_address = entry.data[CONF_IP_ADDRESS]
-    async_add_entities([
+    sensors = [
         LueftungsanlageSensor(ip_address, "aktuell0", "Lüftungsanlage Stufe", entry.entry_id),
         LueftungsanlageSensor(ip_address, "abl0", "Lüftungsanlage Abluft Temperatur", entry.entry_id),
         LueftungsanlageSensor(ip_address, "zul0", "Lüftungsanlage Zuluft Temperatur", entry.entry_id),
@@ -13,9 +14,14 @@ async def async_setup_entry(hass, entry, async_add_entities):
         LueftungsanlageSensor(ip_address, "fol0", "Lüftungsanlage Fortluft Temperatur", entry.entry_id),
         LueftungsanlageSensor(ip_address, "rest_time", "Lüftungsanlage Filter Restzeit", entry.entry_id),
         LueftungsanlageSensor(ip_address, "bypass", "Lüftungsanlage Bypass Status", entry.entry_id)
-    ])
+    ]
+    async_add_entities(sensors)
+    LueftungsanlageSensor.update_all_sensors(sensors)
 
 class LueftungsanlageSensor(SensorEntity):
+    _shared_data = None
+    _last_update = 0
+
     def __init__(self, ip_address, sensor_type, name, entry_id):
         self._ip_address = ip_address
         self._sensor_type = sensor_type
@@ -36,8 +42,23 @@ class LueftungsanlageSensor(SensorEntity):
     def unique_id(self):
         return f"{self._entry_id}_{self._sensor_type}"
 
+    @classmethod
+    def update_all_sensors(cls, sensors):
+        current_time = time.time()
+        if cls._shared_data is None or (current_time - cls._last_update) > 5:
+            res_xml = requests.get(f"http://{sensors[0]._ip_address}/status.xml")
+            if res_xml.status_code == 200:
+                cls._shared_data = xmltodict.parse(res_xml.text)['response']
+                cls._last_update = current_time
+        for sensor in sensors:
+            sensor.update_from_shared_data()
+
+    def update_from_shared_data(self):
+        raw_value = self._shared_data.get(self._sensor_type)
+        if self._sensor_type == "aktuell0" and raw_value and "Stufe" in raw_value:
+            self._state = int(raw_value.split("Stufe")[1].split()[0])
+        else:
+            self._state = raw_value
+
     def update(self):
-        res_xml = requests.get(f"http://{self._ip_address}/status.xml")
-        if res_xml.status_code == 200:
-            res_xml = xmltodict.parse(res_xml.text)
-            self._state = res_xml['response'].get(self._sensor_type)
+        self.update_all_sensors([self])
