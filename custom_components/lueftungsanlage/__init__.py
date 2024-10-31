1a293af8c9c3e4b67b3217dface41f21db35e808
LordZeroMS <github_code@lordzero.de>
2024-10-31 09:57:05 +0100

    fix error from adding sensors #3

--- custom_components/lueftungsanlage/__init__.py
+++ custom_components/lueftungsanlage/__init__.py
@@ -2,7 +2,5 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, ["sensor", "number"])
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "number"])
     return True
--- custom_components/lueftungsanlage/number.py
+++ custom_components/lueftungsanlage/number.py
@@ -4,10 +4,11 @@ from .const import DOMAIN, CONF_IP_ADDRESS
 
 async def async_setup_entry(hass, entry, async_add_entities):
     ip_address = entry.data[CONF_IP_ADDRESS]
-    async_add_entities([LueftungsanlageRegler(ip_address)])
+    async_add_entities([LueftungsanlageRegler(hass, ip_address)])
 
 class LueftungsanlageRegler(NumberEntity):
-    def __init__(self, ip_address):
+    def __init__(self, hass, ip_address):
+        self._hass = hass
         self._ip_address = ip_address
         self._value = 1  # Default stage
 
@@ -33,7 +34,7 @@ class LueftungsanlageRegler(NumberEntity):
 
     async def async_set_value(self, value):
         if 1 <= value <= 4:
-            await hass.async_add_executor_job(
+            await self._hass.async_add_executor_job(
                 requests.get, f"http://{self._ip_address}/stufe.cgi?stufe={value}"
             )
             self._value = value
